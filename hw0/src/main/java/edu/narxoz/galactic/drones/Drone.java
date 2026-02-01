42cb14681601a65ff036f146c957d540ec008b42
markhabaerzhan <markhaba.erzhankyzy@narxoz.kz>
2026-02-02 02:24:48 +0500

    add drones

---
+++ hw0/src/main/java/edu/narxoz/galactic/drones/Drone.java
@@ -0,0 +1,28 @@
+package edu.narxoz.galactic.drones;
+
+public abstract class Drone {
+    private String id;
+    private DroneStatus status;
+    private double maxPayloadKg;
+    protected Drone(String id, double maxPayloadKg){
+        if(maxPayloadKg<=0){
+            throw new IllegalArgumentException("max pay must greater more than 0");
+        }
+        this.id=id;
+        this.maxPayloadKg=maxPayloadKg;
+        this.status=DroneStatus.IDLE;
+    }
+    public String getId(){
+        return id;
+    }
+    public DroneStatus getStatus(){
+        return status;
+    }
+    public double getMaxPayloadKg(){
+        return maxPayloadKg;
+    }
+    protected void setStatus(DroneStatus status){
+        this.status = status;
+    }
+    public abstract double speedKmPerMin();
+}
